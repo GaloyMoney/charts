ecdf778d4ecc52e09814c59f3f9d6cc20b0c3d7f
bodymindarts <justin@galoy.io>
2021-07-21 11:02:24 +0200

    Execute commit in bump-chart

--- ci/tasks/bump-chart.sh
+++ ci/tasks/bump-chart.sh
@@ -1,3 +1,21 @@
 #!/bin/bash
 
-exit 1
+cd galoy-deployments
+
+make bump-chart CHART=${CHART} REF=$(cat ../chart/.git/ref)
+make vendir
+
+if [[ -z $(git config --global user.email) ]]; then
+  git config --global user.email "bot@galoy.io"
+fi
+if [[ -z $(git config --global user.name) ]]; then
+  git config --global user.name "CI Bot"
+fi
+
+
+(cd $(git rev-parse --show-toplevel)
+git merge --no-edit ${BRANCH}
+git add -A
+git status
+git commit -m "$1"
+)
