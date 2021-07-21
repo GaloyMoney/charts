c442601e665a356d00602a096b32e9b42d9eec7a
bodymindarts <justin@galoy.io>
2021-07-21 15:11:42 +0200

    Curl metrics in bitcoind smoketest

--- ci/tasks/bitcoind-smoketest.sh
+++ ci/tasks/bitcoind-smoketest.sh
@@ -3,10 +3,13 @@
 set -eu
 
 namespace=${NAMESPACE:-$(cat testflight/env_name)}
+host=bitcoind.${namespace}.svc.cluster.local
 bitcoin-cli -version
 bitcoin-cli -testnet \
   -rpcuser=rpcuser \
   -rpcpassword=${BITCOIND_RPCPASSWORD} \
   -rpcport=${BITCOIND_RPCPORT} \
-  -rpcconnect=bitcoind.${namespace}.svc.cluster.local \
+  -rpcconnect=${host}\
   -getinfo
+
+curl ${host}:3000/metrics
