6cbdf359aa774c69cc93bf43d63419195383570f
Kartik Shah <krtk.6160@gmail.com>
2021-08-02 19:00:26 +0530

    Put lnd smoke test in a loop

--- ci/tasks/lnd-smoketest.sh
+++ ci/tasks/lnd-smoketest.sh
@@ -5,4 +5,13 @@ set -eu
 namespace=${NAMESPACE:-$(cat testflight/env_name)}
 host=lnd-lndmon.${namespace}.svc.cluster.local
 
-curl ${host}:9092/metrics
+set +e
+for i in {1..60}; do
+  echo "Attempt ${i} to curl lndmon"
+  curl ${host}:9092/metrics
+  if [[ $? == 0 ]]; then success="true"; break; fi;
+  sleep 1
+done
+set -e
+
+if [[ "$success" != "true" ]]; then echo "Smoke test failed" && exit 1; fi;
