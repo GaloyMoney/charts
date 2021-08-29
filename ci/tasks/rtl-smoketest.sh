627ce276ec70c2ae862158f1393252cf98c81f9f
Kartik Shah <krtk.6160@gmail.com>
2021-08-29 12:48:11 +0530

    Update rtl smoketest

--- ci/tasks/rtl-smoketest.sh
+++ ci/tasks/rtl-smoketest.sh
@@ -1,3 +1,18 @@
 #!/bin/bash
 
-exit 0
+set -eu
+
+namespace=${NAMESPACE:-$(cat testflight/env_name)}
+host=${HOST:-rtl.${namespace}.svc.cluster.local}
+port=${PORT:-3000}
+
+set +e
+for i in {1..15}; do
+  echo "Attempt ${i} to curl specter"
+  curl ${host}:${port}
+  if [[ $? == 0 ]]; then success="true"; break; fi;
+  sleep 1
+done
+set -e
+
+if [[ "$success" != "true" ]]; then echo "Smoke test failed" && exit 1; fi;
