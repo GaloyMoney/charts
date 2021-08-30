8bba07fad7924410ac84fb6351aaa9c6100aaf7f
Kartik Shah <krtk.6160@gmail.com>
2021-08-30 16:00:28 +0530

    Revert "Add loop smoke test"
    This reverts commit 279e96370ceafb7d4c5331db03f9eecc57de7abe.

--- ci/tasks/lnd-smoketest.sh
+++ ci/tasks/lnd-smoketest.sh
@@ -3,15 +3,12 @@
 set -eu
 
 namespace=${NAMESPACE:-$(cat testflight/env_name)}
-lndmonHost=lnd-lndmon.${namespace}.svc.cluster.local
-loopHost=lnd-loop.${namespace}.svc.cluster.local
+host=lnd-lndmon.${namespace}.svc.cluster.local
 
 set +e
 for i in {1..60}; do
-  echo "Attempt ${i} to curl lndmon and loop"
-  curl ${lndmonHost}:9092/metrics
-  if [[ $? != 0 ]]; then continue; fi;
-  curl ${loopHost}:8081/v1/auto/suggest
+  echo "Attempt ${i} to curl lndmon"
+  curl ${host}:9092/metrics
   if [[ $? == 0 ]]; then success="true"; break; fi;
   sleep 1
 done
