82e52356c4c9d4e9e4d73797f2b41409a02402a7
Kartik Shah <krtk.6160@gmail.com>
2021-09-03 19:51:16 +0530

    Update galoy smoketest

--- ci/tasks/galoy-smoketest.sh
+++ ci/tasks/galoy-smoketest.sh
@@ -1,3 +1,8 @@
 #!/bin/bash
 
-exit 1
+namespace=${NAMESPACE:-$(cat testflight/env_name)}
+host=graphql.${namespace}.svc.cluster.local
+
+curl --location --request POST '${host}:4000/graphql'\
+ --header 'Content-Type: application/json' \
+ --data-raw '{"query":"query prices {\n    prices(length: 0) {\n        id\n        o\n    }\n}","variables":{}}'
