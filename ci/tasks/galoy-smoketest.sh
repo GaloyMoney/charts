91c8c27ffdcaf0ea2b958ae540299bd4b7b22eba
bodymindarts <justin@galoy.io>
2021-09-13 14:17:59 +0200

    Make smoketest fail

--- ci/tasks/galoy-smoketest.sh
+++ ci/tasks/galoy-smoketest.sh
@@ -5,4 +5,10 @@ host=graphql.${namespace}.svc.cluster.local
 
 curl --location --request POST "${host}:4000/graphql"\
  --header 'Content-Type: application/json' \
- --data-raw '{"query":"query prices {\n    prices(length: 0) {\n        id\n        o\n    }\n}","variables":{}}'
+ --data-raw '{"query":"query prices {\n    prices(length: 0) {\n        id\n        o\n    }\n}","variables":{}}' > response.json
+
+if [[ $(cat ./response.json | jq -r '.errors') != "null" ]]; then
+  echo Testflight failed! - Response:
+  cat response.json
+  echo contained "errors" key
+fi
