33a8327e71a0feced379506d586893f54833ab09
Kartik Shah <krtk.6160@gmail.com>
2021-09-22 00:25:33 +0530

    Update monitoring smoke test

--- ci/tasks/monitoring-smoketest.sh
+++ ci/tasks/monitoring-smoketest.sh
@@ -3,6 +3,7 @@
 set -eu
 
 namespace=${NAMESPACE:-$(cat testflight/env_name)}
-host=${namespace}-prometheus-alertmanager.${namespace}.svc.cluster.local
+alertmanager_dns=${ALERTMANAGER_DNS:-"${namespace}-prometheus-alertmanager"}
+host=${alertmanager_dns}.${namespace}.svc.cluster.local
 
 curl ${host}/-/healthy
