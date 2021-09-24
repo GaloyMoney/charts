c6ee12170b25111051af1b9591431378a0443061
bodymindarts <justin@galoy.io>
2021-09-25 01:25:46 +0200

    Another OUT in get-settings

--- ci/tasks/get-smoketest-settings.sh
+++ ci/tasks/get-smoketest-settings.sh
@@ -9,7 +9,7 @@ echo ${SMOKETEST_KUBECONFIG} | base64 --decode > ${KUBECONFIG}
 kubectl get secret ${SMOKETEST_SECRET:-$(cat testflight/env_name)} -o json \
   | jq -r '.data' > ${OUT}/data.json
 
-cat <<EOF > smoketest-settings/helpers.sh
+cat <<EOF > ${OUT}/helpers.sh
 function setting() {
   cat smoketest-settings/data.json | jq -r ".\$1" | base64 --decode
 }
