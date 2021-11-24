7c89f78605e2ee4c657beac7c014a2c62d44317d
Kartik Shah <krtk.6160@gmail.com>
2021-11-24 11:15:14 +0530

    Revert "Disable dealer testflight temporarily"
    This reverts commit 35d14e9f9436879388ad1ba496e300ba94f9976c.

--- ci/tasks/dealer-smoketest.sh
+++ ci/tasks/dealer-smoketest.sh
@@ -1,21 +1,19 @@
 #!/bin/bash
 
-exit 0
+set -eu
 
-# set -eu
+source smoketest-settings/helpers.sh
 
-# source smoketest-settings/helpers.sh
+host=`setting "dealer_endpoint"`
+port=`setting "dealer_port"`
 
-# host=`setting "dealer_endpoint"`
-# port=`setting "dealer_port"`
+set +e
+for i in {1..15}; do
+  echo "Attempt ${i} to curl dealer's /metrics endpoint"
+  curl ${host}:${port}/metrics
+  if [[ $? == 0 ]]; then success="true"; break; fi;
+  sleep 1
+done
+set -e
 
-# set +e
-# for i in {1..15}; do
-#   echo "Attempt ${i} to curl dealer's /metrics endpoint"
-#   curl ${host}:${port}/metrics
-#   if [[ $? == 0 ]]; then success="true"; break; fi;
-#   sleep 1
-# done
-# set -e
-
-# if [[ "$success" != "true" ]]; then echo "Smoke test failed" && exit 1; fi;
+if [[ "$success" != "true" ]]; then echo "Smoke test failed" && exit 1; fi;
