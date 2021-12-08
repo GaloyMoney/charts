febe21897c45a080ffdc57dbefe01035abcd8dde
Kartik Shah <krtk.6160@gmail.com>
2021-12-08 20:04:01 +0530

    Add jupyterhub dev

---
+++ dev/addons/jupyterhub.tf
@@ -0,0 +1,9 @@
+resource "helm_release" "jupyterhub" {
+  name      = "jupyter"
+  chart     = "jupyterhub/jupyterhub"
+  namespace = kubernetes_namespace.data.metadata[0].name
+
+  values = [
+    file("${path.module}/jupyterhub-values.yml")
+  ]
+}
