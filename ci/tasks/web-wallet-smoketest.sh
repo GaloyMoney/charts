9f4bd2dd01d8e2fb4e27ae8101e4618c47bca49d
Kartik Shah <krtk.6160@gmail.com>
2022-02-01 14:27:31 +0530

    ci: Add web wallet testflight (#540)
    * ci: Add web wallet testflight
    
    * fix web wallet smoketest endpoint

---
+++ ci/tasks/web-wallet-smoketest.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+set -eu
+
+source smoketest-settings/helpers.sh
+
+host=`setting "web_wallet_endpoint"`
+port=`setting "web_wallet_port"`
+
+set +e
+for i in {1..15}; do
+  echo "Attempt ${i} to curl web wallet"
+  curl ${host}:${port}
+  if [[ $? == 0 ]]; then success="true"; break; fi;
+  sleep 1
+done
+set -e
+
+if [[ "$success" != "true" ]]; then echo "Smoke test failed" && exit 1; fi;
