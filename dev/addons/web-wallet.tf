267f6c6d3e557508b786c591e37a042cae51fa0d
Kartik Shah <krtk.6160@gmail.com>
2022-02-01 14:00:45 +0530

    feat: add web wallet chart (#539)
    * feat: add web wallet chart

---
+++ dev/addons/web-wallet.tf
@@ -0,0 +1,23 @@
+locals {
+  session_keys = "session_keys"
+}
+
+resource "kubernetes_secret" "web_wallet_secret" {
+  metadata {
+    name      = "web-wallet"
+    namespace = kubernetes_namespace.addons.metadata[0].name
+  }
+
+  data = {
+    "session-keys" : local.session_keys
+  }
+}
+
+resource "helm_release" "web_wallet" {
+  name       = "web-wallet"
+  chart      = "${path.module}/../../charts/web-wallet"
+  repository = "https://galoymoney.github.io/charts/"
+  namespace  = kubernetes_namespace.addons.metadata[0].name
+
+  depends_on = [kubernetes_secret.web_wallet_secret]
+}
