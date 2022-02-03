3549fecbf230e48d059fef3087e7cdab6cf41975
Justin Carter <justin@galoy.io>
2022-02-03 10:36:01 +0100

    chore!: remove support for old api (#557)
    * chore!: remove support for old api
    
    * fix: update smoketest to hit correct api

--- ci/tasks/galoy-smoketest.sh
+++ ci/tasks/galoy-smoketest.sh
@@ -7,7 +7,7 @@ port=`setting "galoy_port"`
 
 curl --location --request POST "${host}:${port}/graphql"\
  --header 'Content-Type: application/json' \
- --data-raw '{"query":"query prices {\n    prices(length: 0) {\n        id\n        o\n    }\n}","variables":{}}' > response.json
+ --data-raw '{"query":"query btcPriceList($range: PriceGraphRange!) { btcPriceList(range: $range) { timestamp price { base offset currencyUnit formattedAmount } } }","variables":{"range":"ONE_DAY"}}' > response.json
 
 if [[ $(cat ./response.json | jq -r '.errors') != "null" ]]; then
   echo Testflight failed! - Response:
