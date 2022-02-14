f925ea41b071367c0081cfb6641e7d5ab41ea2bc
Kartik Shah <krtk.6160@gmail.com>
2022-02-14 14:26:02 +0530

    chore: add galoy-auth smoketest

---
+++ ci/tasks/galoy-auth-smoketest.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+set -eu
+
+source smoketest-settings/helpers.sh
+
+host=`setting "galoy_auth_endpoint"`
+port=`setting "galoy_auth_port"`
+
+set +e
+for i in {1..15}; do
+  echo "Attempt ${i} to curl galoy-auth"
+  curl ${host}:${port}
+  if [[ $? == 0 ]]; then success="true"; break; fi;
+  sleep 1
+done
+set -e
+
+if [[ "$success" != "true" ]]; then echo "Smoke test failed" && exit 1; fi;
