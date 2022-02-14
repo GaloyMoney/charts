4a298f8003f02ed322c68c94576d3c1272f2f0b1
Kartik Shah <krtk.6160@gmail.com>
2022-02-14 13:50:09 +0530

    feat: add galoy-auth to local dev

---
+++ dev/auth/main.tf
@@ -0,0 +1,32 @@
+variable "name_prefix" {}
+
+locals {
+  auth_namespace = "${var.name_prefix}-addons"
+  
+  session_keys = "session_keys"
+}
+
+resource "kubernetes_namespace" "auth" {
+  metadata {
+    name = local.auth_namespace
+  }
+}
+
+resource "kubernetes_secret" "auth_backend_secret" {
+  metadata {
+    name      = "auth-backend"
+    namespace = kubernetes_namespace.addons.metadata[0].name
+  }
+
+  data = {
+    "session-keys" : local.session_keys
+  }
+}
+
+resource "helm_release" "galoy_auth" {
+  name       = "galoy-auth"
+  chart      = "${path.module}/../../charts/galoy-auth"
+  namespace  = kubernetes_namespace.auth.metadata[0].name
+
+  depends_on = [kubernetes_secret.auth_backend_secret]
+}
