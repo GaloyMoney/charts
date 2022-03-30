9f36c67361ecd04f8bed2db5c382c2b6d5fdae43
Sandipan Dey <hey@sandipan.dev>
2022-03-30 16:36:26 +0530

    ci: add bump pr task

--- ci/tasks/update-helm-deps.sh
+++ ci/tasks/update-helm-deps.sh
@@ -40,3 +40,18 @@ for d in */ ; do
   popd
 
 done
+
+if [[ -z $(git config --global user.email) ]]; then
+  git config --global user.email "bot@galoy.io"
+fi
+if [[ -z $(git config --global user.name) ]]; then
+  git config --global user.name "CI Bot"
+fi
+
+
+(cd $(git rev-parse --show-toplevel)
+git merge --no-edit ${BRANCH}
+git add -A
+git status
+git commit -m "chore(deps): update helm chart dependencies"
+)
