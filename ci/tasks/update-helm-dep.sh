714efb446a61a7d984c13ddd1de5dc22cd8742bb
Sandipan Dey <hey@sandipan.dev>
2022-04-05 13:12:33 +0530

    ci: fix update helm dep script

--- ci/tasks/update-helm-dep.sh
+++ ci/tasks/update-helm-dep.sh
@@ -1,10 +1,12 @@
 #!/bin/bash
 
+set -eu
+
 VERSION=$(cat helm-resource/version)
 
 pushd charts-repo/charts/$DIR
 
-yq -i "(.dependencies[] | select(.name == "$DEP") | .version) = \"$VERSION\"" Chart.yaml
+yq -i "(.dependencies[] | select(.name == \"$DEP\") | .version) = \"$VERSION\"" Chart.yaml
 
 helm dependency update
 
@@ -15,9 +17,12 @@ if [[ -z $(git config --global user.name) ]]; then
   git config --global user.name "CI Bot"
 fi
 
-(cd $(git rev-parse --show-toplevel)
+cd $(git rev-parse --show-toplevel)
 git merge --no-edit ${BRANCH}
 git add -A
 git status
-git commit -m "chore(deps): update $DEP helm chart in $DIR"
-)
+
+# Only commit if there are uncommitted staged files
+if ! git diff --cached --exit-code; then
+  git commit -m "chore(deps): update $DEP helm chart in $DIR"
+fi
