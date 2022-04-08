6ddae357855d0f03d11acb2c68c52ce5e0eb2b9e
Kartik Shah <krtk.6160@gmail.com>
2022-04-08 19:50:03 +0530

    feat: add price history smoketest (#816)
    * feat: add price history smoketest
    
    * chore: add comment referencing original health.proto file

--- ci/tasks/galoy-smoketest.sh
+++ ci/tasks/galoy-smoketest.sh
@@ -41,3 +41,45 @@ if [[ `setting_exists "smoketest_kubeconfig"` != "null" ]]; then
   fi
   kubectl -n ${namespace} delete job "${job_name}"
 fi
+
+# The following health.proto file has been copied from
+# https://github.com/GaloyMoney/price/blob/main/history/src/servers/protos/health.proto
+cat << EOF > health.proto
+syntax = "proto3";
+
+package grpc.health.v1;
+
+message HealthCheckRequest {
+  string service = 1;
+}
+
+message HealthCheckResponse {
+  enum ServingStatus {
+    UNKNOWN = 0;
+    SERVING = 1;
+    NOT_SERVING = 2;
+    SERVICE_UNKNOWN = 3;  // Used only by the Watch method.
+  }
+  ServingStatus status = 1;
+}
+
+service Health {
+  rpc Check(HealthCheckRequest) returns (HealthCheckResponse);
+
+  rpc Watch(HealthCheckRequest) returns (stream HealthCheckResponse);
+}
+EOF
+
+host=`setting "price_history_endpoint"`
+port=`setting "price_history_port"`
+
+set +e
+for i in {1..15}; do
+  echo "Attempt ${i} to curl price history server"
+  grpcurl -plaintext -proto health.proto ${host}:${port} grpc.health.v1.Health.Check
+  if [[ $? == 0 ]]; then success="true"; break; fi;
+  sleep 1
+done
+set -e
+
+if [[ "$success" != "true" ]]; then echo "Smoke test failed" && exit 1; fi;
