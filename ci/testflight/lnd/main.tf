2176bcc05cb759e4983a45fbc1b4c51d30d25019
Sandipan Dey <hey@sandipan.dev>
2022-06-10 14:49:06 +0530

    ci: add pg secret on testflight

--- ci/testflight/lnd/main.tf
+++ ci/testflight/lnd/main.tf
@@ -58,6 +58,18 @@ resource "kubernetes_secret" "smoketest" {
   }
 }
 
+resource "kubernetes_secret" "lnd_pg_pass" {
+  metadata {
+    name      = "postgres-creds"
+    namespace = kubernetes_namespace.bitcoin.metadata[0].name
+  }
+
+  data = {
+    uri = "postgres://postgres:password@lnd1-postgresql:5432/lnd"
+    "postgres-password" = "password"
+  }
+}
+
 resource "helm_release" "lnd1" {
   name       = "lnd1"
   chart      = "${path.module}/chart"
