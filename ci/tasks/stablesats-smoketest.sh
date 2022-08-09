5026444eeb44f408317f74dcbcc1fa1b3211f5c4
bodymindarts <justin@galoy.io>
2022-08-09 16:07:04 +0200

    ci: fix stablesats-smoketest

--- ci/tasks/stablesats-smoketest.sh
+++ ci/tasks/stablesats-smoketest.sh
@@ -6,13 +6,14 @@ source smoketest-settings/helpers.sh
 
 price_host=`setting "price_server_grpc_host"`
 price_port=`setting "price_server_grpc_port"`
+export PRICE_SERVER_URL="http://${price_host}:${price_port}"
 
 set +e
 for i in {1..15}; do
   echo "Attempt ${i} to get a quote"
-  # curl ${host}:${port}/metrics
-  # if [[ $? == 0 ]]; then success="true"; break; fi;
-  # sleep 1
+  stablesats price 100000000
+  if [[ $? == 0 ]]; then success="true"; break; fi;
+  sleep 1
 done
 set -e
 
