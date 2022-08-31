08bb00ea0f23ab2fc0cf4590778042e27c4dcc9f
Kartik Shah <krtk.6160@gmail.com>
2022-08-31 16:17:49 +0530

    fix: readd dealer creds to testflight

--- ci/testflight/dealer/main.tf
+++ ci/testflight/dealer/main.tf
@@ -9,6 +9,8 @@ locals {
   galoy_namespace      = "galoy-staging-galoy"
   testflight_namespace = var.testflight_namespace
 
+  postgres_password   = "postgres"
+  postgres_db_uri     = "postgres://postgres:postgres@dealer-postgresql:5432/dealer"
   okex5_key           = "key"
   okex5_secret        = "secret"
   okex5_password      = "pwd"
@@ -32,6 +34,18 @@ resource "kubernetes_secret" "okex5_creds" {
     "okex5_secret" : local.okex5_secret
     "okex5_password" : local.okex5_password
     "okex5_fund_password" : local.okex5_fund_password
+  }
+}
+
+resource "kubernetes_secret" "postgres_creds" {
+  metadata {
+    name      = "dealer-postgres"
+    namespace = local.testflight_namespace
+  }
+
+  data = {
+    "postgresql-password" : local.postgres_password
+    "postgresql-db-uri" : local.postgres_db_uri
   }
 }
 
