9237890fe99d38519d349d3cb0feff3da893883e
Enigbe OS <ochekliyeenigbe@gmail.com>
2022-09-14 15:39:12 +0100

    fix(smoketest): follow redirects (#1587)

--- ci/tasks/galoy-auth-smoketest.sh
+++ ci/tasks/galoy-auth-smoketest.sh
@@ -10,7 +10,7 @@ port=`setting "galoy_auth_port"`
 set +e
 for i in {1..15}; do
   echo "Attempt ${i} to curl galoy-auth"
-  curl ${host}:${port}
+  curl --location ${host}:${port}
   if [[ $? == 0 ]]; then success="true"; break; fi;
   sleep 1
 done
