47cccfa4cb52fa54dfc3396a95e2f780ce0744f9
Kartik Shah <krtk.6160@gmail.com>
2022-10-18 12:21:21 +0530

    chore: add galoy-deps testflight (#1845)

---
+++ ci/testflight/galoy-deps/main.tf
@@ -0,0 +1,7 @@
+resource "helm_release" "galoy_deps" {
+  name      = "galoy-deps"
+  chart     = "${path.module}/chart"
+  namespace = kubernetes_namespace.testflight.metadata[0].name
+
+  dependency_update = true
+}
