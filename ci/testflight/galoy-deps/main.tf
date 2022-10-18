efba44dd2511758ed650b755a475d4115e0e77e8
Kartik Shah <krtk.6160@gmail.com>
2022-10-18 13:23:14 +0530

    fix: add missing providers to deps testflight (#1848)

--- ci/testflight/galoy-deps/main.tf
+++ ci/testflight/galoy-deps/main.tf
@@ -1,6 +1,10 @@
 variable "testflight_namespace" {}
 
 locals {
+  cluster_name     = "galoy-staging-cluster"
+  cluster_location = "us-east1"
+  gcp_project      = "galoy-staging"
+
   testflight_namespace = var.testflight_namespace
 }
 
@@ -16,4 +20,34 @@ resource "helm_release" "galoy_deps" {
   namespace = kubernetes_namespace.testflight.metadata[0].name
 
   dependency_update = true
+}
+
+data "google_container_cluster" "primary" {
+  project  = local.gcp_project
+  name     = local.cluster_name
+  location = local.cluster_location
+}
+
+data "google_client_config" "default" {
+  provider = google-beta
+}
+
+provider "kubernetes" {
+  host                   = "https://${data.google_container_cluster.primary.private_cluster_config.0.private_endpoint}"
+  token                  = data.google_client_config.default.access_token
+  cluster_ca_certificate = base64decode(data.google_container_cluster.primary.master_auth.0.cluster_ca_certificate)
+}
+
+provider "kubernetes-alpha" {
+  host                   = "https://${data.google_container_cluster.primary.private_cluster_config.0.private_endpoint}"
+  token                  = data.google_client_config.default.access_token
+  cluster_ca_certificate = base64decode(data.google_container_cluster.primary.master_auth.0.cluster_ca_certificate)
+}
+
+provider "helm" {
+  kubernetes {
+    host                   = "https://${data.google_container_cluster.primary.private_cluster_config.0.private_endpoint}"
+    token                  = data.google_client_config.default.access_token
+    cluster_ca_certificate = base64decode(data.google_container_cluster.primary.master_auth.0.cluster_ca_certificate)
+  }
 }
