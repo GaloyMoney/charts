d8623f085d9037ad0f77eeafdd64aaec762e2aee
Your Name <you@example.com>
2022-10-25 19:52:45 +0100

    remove empty values.yml

--- dev/bitcoin/rtl.tf
+++ dev/bitcoin/rtl.tf
@@ -1,12 +1,9 @@
 resource "helm_release" "rtl" {
-  name       = "rtl"
-  chart      = "${path.module}/../../charts/rtl"
-  namespace  = kubernetes_namespace.bitcoin.metadata[0].name
+  name      = "rtl"
+  chart     = "${path.module}/../../charts/rtl"
+  namespace = kubernetes_namespace.bitcoin.metadata[0].name
 
   dependency_update = true
-  values = [
-    file("${path.module}/rtl-values.yml")
-  ]
 
   depends_on = [
     helm_release.lnd
