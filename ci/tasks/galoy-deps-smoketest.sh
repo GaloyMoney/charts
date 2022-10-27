2ebc00e73a30c7d0f3f26ad955f0b1ec473b3a93
Kartik Shah <krtk.6160@gmail.com>
2022-10-27 12:47:44 +0530

    refactor: add multiple tries to deps smoketest (#1926)

--- ci/tasks/galoy-deps-smoketest.sh
+++ ci/tasks/galoy-deps-smoketest.sh
@@ -8,11 +8,17 @@ kafka_broker_host=`setting "kafka_broker_endpoint"`
 kafka_broker_port=`setting "kafka_broker_port"`
 kafka_topic=`setting "kafka_topic"`
 
-set +e
 
 msg="kafka message"
-echo $msg | kafkacat -P -b $kafka_broker_host:$kafka_broker_port -t $kafka_topic
-consumed_message=$(kafkacat -C -b $kafka_broker_host:$kafka_broker_port -t $kafka_topic)
-if [[ "$consumed_message" == "$msg" ]]; then exit 0; else echo "Smoke test failed" && exit 1; fi;
+set +e
+for i in {1..15}; do
+  echo "Attempt ${i} to produce and consume from kafka"
+  echo $msg | kafkacat -P -b $kafka_broker_host:$kafka_broker_port -t $kafka_topic
+  consumed_message=$(kafkacat -C -b $kafka_broker_host:$kafka_broker_port -t $kafka_topic -e)
+  if [[ "$consumed_message" == "$msg" ]]; then success="true"; break; fi;
+  sleep 1
+done
+
+if [[ "$success" != "true" ]]; then echo "Smoke test failed" && exit 1; fi;
 
 set -e
