b3d92b1e61ce79294390992bc92f942591d3211a
openoms <oms@tuta.io>
2022-10-27 08:30:01 +0100

    merge signer.tf to main.tf

--- dev/signer/main.tf
+++ dev/signer/main.tf
@@ -10,3 +10,15 @@ resource "kubernetes_namespace" "signer" {
     name = local.signer_namespace
   }
 }
+
+resource "helm_release" "signer" {
+  name      = "signer1"
+  chart     = "${path.module}/../../charts/signer"
+  namespace = kubernetes_namespace.signer.metadata[0].name
+
+  dependency_update = true
+  timeout           = 300
+  values = [
+    file("${path.module}/signer-${var.bitcoin_network}-values.yml")
+  ]
+}
