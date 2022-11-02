aed7815b3039642fed6f74a6e0e93151f55f4dde
openoms <43343391+openoms@users.noreply.github.com>
2022-11-02 13:44:00 +0000

    chore!: switch bitcoin smoketest to signet (#1967)

--- ci/tasks/bitcoind-smoketest.sh
+++ ci/tasks/bitcoind-smoketest.sh
@@ -6,7 +6,7 @@ source smoketest-settings/helpers.sh
 
 host=`setting "bitcoind_endpoint"`
 bitcoin-cli -version
-bitcoin-cli -${1:-testnet} \
+bitcoin-cli -${1:-signet} \
   -rpcuser=`setting "bitcoind_user"` \
   -rpcpassword=`setting "bitcoind_rpcpassword"` \
   -rpcport=`setting "bitcoind_port"` \
