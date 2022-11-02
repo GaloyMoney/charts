b9df608981f1587a1f655c72f1951d00f596ed63
Kartik Shah <krtk.6160@gmail.com>
2022-11-02 19:29:07 +0530

    ci: remove deps testflight custom values

--- ci/testflight/galoy-deps/main.tf
+++ ci/testflight/galoy-deps/main.tf
@@ -23,12 +23,6 @@ resource "helm_release" "galoy_deps" {
   chart     = "${path.module}/chart"
   namespace = kubernetes_namespace.testflight.metadata[0].name
 
-  values = [
-    templatefile("${path.module}/galoy-deps-values.yml.tmpl", {
-      watch_namespaces : ["${local.smoketest_namespace}"]
-    })
-  ]
-
   dependency_update = true
 }
 
