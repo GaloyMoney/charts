2d88e3f0386fbf63b9228af6a5966c5dbbc7908f
Kartik Shah <krtk.6160@gmail.com>
2022-11-24 21:31:00 +0530

    refactor: galoy deps in dev

---
+++ dev/galoy-deps/kafka.tf
@@ -0,0 +1,27 @@
+variable "watch_namespaces" {
+  default = []
+}
+
+resource "kubernetes_namespace" "kafka" {
+  metadata {
+    name = "kafka"
+  }
+}
+
+locals {
+  watch_namespaces = var.watch_namespaces
+}
+
+resource "helm_release" "kafka" {
+  name      = "kafka"
+  chart     = "${path.module}/../../charts/galoy-deps"
+  namespace = kubernetes_namespace.kafka.metadata[0].name
+
+  values = [
+    templatefile("${path.module}/kafka-values.yml.tmpl", {
+      watch_namespaces : local.watch_namespaces
+    })
+  ]
+
+  dependency_update = true
+}
---
+++ dev/galoy-deps/kubemonkey.tf
@@ -0,0 +1,33 @@
+resource "kubernetes_namespace" "kubemonkey" {
+  metadata {
+    name = "kubemonkey"
+  }
+}
+
+locals {
+  ns1 = "test"
+  ns2 = "test2"
+
+  kubemonkey_time_zone = "Etc/UTC"
+  kubemonkey_notification_url = "dummy"
+  kubemonkey_whitelisted_namespaces = [
+    local.ns1,
+    local.ns2
+  ]
+}
+
+resource "helm_release" "kubemonkey" {
+  name      = "kubemonkey"
+  chart     = "${path.module}/../../charts/galoy-deps"
+  namespace = kubernetes_namespace.kubemonkey.metadata[0].name
+
+  values = [
+    templatefile("${path.module}/kubemonkey-values.yml.tmpl", {
+      time_zone : local.kubemonkey_time_zone
+      whitelisted_namespaces : local.kubemonkey_whitelisted_namespaces
+      notification_url : local.kubemonkey_notification_url
+    })
+  ]
+
+  dependency_update = true
+}
