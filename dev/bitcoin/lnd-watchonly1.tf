f3270d3ce5d6fb72383a786c0dac265807605acc
Your Name <you@example.com>
2022-11-30 19:31:24 +0000

    activate lnd-watchonly in dev setup

---
+++ dev/bitcoin/lnd-watchonly1.tf
@@ -0,0 +1,50 @@
+data "kubernetes_secret" "signer_credentials" {
+  metadata {
+    name      = "lnd1-signer-credentials"
+    namespace = local.signer_namespace
+  }
+}
+
+resource "kubernetes_secret" "signer_credentials" {
+  metadata {
+    name      = "lnd1-signer-credentials"
+    namespace = local.bitcoin_namespace
+  }
+
+  data = data.kubernetes_secret.signer_credentials.data
+}
+
+data "kubernetes_secret" "signer_accounts" {
+  metadata {
+    name      = "lnd1-signer-accounts"
+    namespace = local.signer_namespace
+  }
+}
+
+resource "kubernetes_secret" "signer_accounts" {
+  metadata {
+    name      = "lnd1-signer-accounts"
+    namespace = local.bitcoin_namespace
+  }
+
+  data = data.kubernetes_secret.signer_accounts.data
+}
+
+resource "helm_release" "lnd-watchonly" {
+  name      = "lnd1"
+  chart     = "${path.module}/../../charts/lnd-watchonly"
+  namespace = kubernetes_namespace.bitcoin.metadata[0].name
+
+  dependency_update = true
+  timeout           = local.bitcoin_network == "regtest" ? 900 : 9000
+  values = [
+    file("${path.module}/lnd-watchonly-${var.bitcoin_network}-values.yml")
+  ]
+
+  depends_on = [
+    kubernetes_secret.bitcoind,
+    helm_release.bitcoind,
+    kubernetes_secret.signer_credentials,
+    kubernetes_secret.signer_accounts
+  ]
+}
--- dev/bitcoin/lnd1.tf
+++ dev/bitcoin/lnd1.tf
@@ -1,62 +1,29 @@
-resource "kubernetes_secret" "lnd_pg_pass" {
-  metadata {
-    name      = "postgres-creds"
-    namespace = kubernetes_namespace.bitcoin.metadata[0].name
-  }
-
-  data = {
-    uri                 = "postgres://postgres:password@lnd1-postgresql:5432/lnd"
-    "postgres-password" = "password"
-  }
-}
-
-data "kubernetes_secret" "signer_credentials" {
-  metadata {
-    name      = "lnd1-signer-credentials"
-    namespace = local.signer_namespace
-  }
-}
-
-resource "kubernetes_secret" "signer_credentials" {
-  metadata {
-    name      = "lnd1-signer-credentials"
-    namespace = local.bitcoin_namespace
-  }
-
-  data = data.kubernetes_secret.signer_credentials.data
-}
-
-data "kubernetes_secret" "signer_accounts" {
-  metadata {
-    name      = "lnd1-signer-accounts"
-    namespace = local.signer_namespace
-  }
-}
-
-resource "kubernetes_secret" "signer_accounts" {
-  metadata {
-    name      = "lnd1-signer-accounts"
-    namespace = local.bitcoin_namespace
-  }
-
-  data = data.kubernetes_secret.signer_accounts.data
-}
-
-resource "helm_release" "lnd" {
-  name      = "lnd1"
-  chart     = "${path.module}/../../charts/lnd"
-  namespace = kubernetes_namespace.bitcoin.metadata[0].name
-
-  dependency_update = true
-  timeout           = local.bitcoin_network == "regtest" ? 900 : 9000
-  values = [
-    file("${path.module}/lnd-${var.bitcoin_network}-values.yml")
-  ]
-
-  depends_on = [
-    kubernetes_secret.bitcoind,
-    helm_release.bitcoind,
-    kubernetes_secret.signer_credentials,
-    kubernetes_secret.signer_accounts
-  ]
-}
+#resource "kubernetes_secret" "lnd_pg_pass" {
+#  metadata {
+#    name      = "postgres-creds"
+#    namespace = kubernetes_namespace.bitcoin.metadata[0].name
+#  }
+#
+#  data = {
+#    uri                 = "postgres://postgres:password@lnd1-postgresql:5432/lnd"
+#    "postgres-password" = "password"
+#  }
+#}
+#
+#resource "helm_release" "lnd" {
+#  name      = "lnd1"
+#  chart     = "${path.module}/../../charts/lnd"
+#  namespace = kubernetes_namespace.bitcoin.metadata[0].name
+#
+#  dependency_update = true
+#  timeout           = local.bitcoin_network == "regtest" ? 900 : 9000
+#  values = [
+#    file("${path.module}/lnd-${var.bitcoin_network}-values.yml")
+#  ]
+#
+#  depends_on = [
+#    kubernetes_secret.bitcoind,
+#    helm_release.bitcoind
+#  ]
+#}
+#
