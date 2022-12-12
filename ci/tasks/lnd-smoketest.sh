6727a5b9baeecc58cc562177ba73ce48497e7d83
openoms <oms@tuta.io>
2022-12-12 19:43:09 +0000

    test the lnd_api_endpoint before lnd_p2p_endpoint

--- ci/tasks/lnd-smoketest.sh
+++ ci/tasks/lnd-smoketest.sh
@@ -4,17 +4,31 @@ set -eu
 
 source smoketest-settings/helpers.sh
 
-lnd_host=`setting "lnd_p2p_endpoint"`
+lnd_api_host=$(setting "lnd_api_endpoint")
+lnd_p2p_host=$(setting "lnd_p2p_endpoint")
 
-if [[ "${lnd_host}" != "" ]]; then
+if [ "${lnd_api_host}" != "" ]; then
   set +e
   for i in {1..60}; do
-    echo "Attempt ${i} to connect the lnd_p2p_endpoint"
-    nc -zv ${lnd_host} 9735
-    if [[ $? == 0 ]]; then success="true"; break; fi;
+    echo "Attempt ${i} to connect the lnd_api_endpoint"
+    nc -zv ${lnd_api_host} 10009
+    if [ $? = 0 ]; then success="true"; break; fi;
       sleep 1
     done
     set -e
 
-    if [[ "$success" != "true" ]]; then echo "Smoke test failed" && exit 1; fi;
+    if [ "$success" != "true" ]; then echo "Could not connect to the lnd_api_endpoint" && exit 1; fi;
+fi
+
+if [ "${lnd_p2p_host}" != "" ]; then
+  set +e
+  for i in {1..60}; do
+    echo "Attempt ${i} to connect the lnd_p2p_endpoint"
+    nc -zv ${lnd_p2p_host} 9735
+    if [ $? = 0 ]; then success="true"; break; fi;
+      sleep 1
+    done
+    set -e
+
+    if [ "$success" != "true" ]; then echo "Could not connect to the lnd_p2p_endpoint" && exit 1; fi;
 fi
--- dev/bitcoin/main.tf
+++ dev/bitcoin/main.tf
@@ -44,6 +44,7 @@ resource "kubernetes_secret" "lnd_smoketest" {
     namespace = local.smoketest_namespace
   }
   data = {
+    lnd_api_endpoint = "lnd1.${local.bitcoin_namespace}.svc.cluster.local"
     lnd_p2p_endpoint = "lnd1-p2p.${local.bitcoin_namespace}.svc.cluster.local"
   }
 }
