f29d5de35bfa8fe07b792481f2c08714617c43b1
openoms <43343391+openoms@users.noreply.github.com>
2023-01-04 18:50:08 +0000

    merge lnd-watchonly and lnd-signer to the lnd-segregated chart (#2313)
    * rename the lnd-watchonly folder to lnd-segregated
    * move signer chart to lnd-segregated
    * segregate templates
    * correct names and format
    * always create lnd-pass when watchonly
    * add note to Makefile

--- dev/bitcoin/lnd1.tf
+++ dev/bitcoin/lnd1.tf
@@ -1,47 +1,3 @@
-resource "kubernetes_secret" "lnd_pg_pass" {
-  metadata {
-    name      = "postgres-creds"
-    namespace = kubernetes_namespace.bitcoin.metadata[0].name
-  }
-
-  data = {
-    uri                 = "postgres://postgres:password@lnd1-postgresql:5432/lnd"
-    "postgres-password" = "password"
-  }
-}
-
-data "kubernetes_secret" "signer_credentials" {
-  metadata {
-    name      = "lnd-signer2-credentials"
-    namespace = local.signer_namespace
-  }
-}
-
-resource "kubernetes_secret" "signer_credentials" {
-  metadata {
-    name      = "lnd-signer2-credentials"
-    namespace = local.bitcoin_namespace
-  }
-
-  data = data.kubernetes_secret.signer_credentials.data
-}
-
-data "kubernetes_secret" "signer_accounts" {
-  metadata {
-    name      = "lnd-signer2-accounts"
-    namespace = local.signer_namespace
-  }
-}
-
-resource "kubernetes_secret" "signer_accounts" {
-  metadata {
-    name      = "lnd-signer2-accounts"
-    namespace = local.bitcoin_namespace
-  }
-
-  data = data.kubernetes_secret.signer_accounts.data
-}
-
 resource "helm_release" "lnd" {
   name      = "lnd1"
   chart     = "${path.module}/../../charts/lnd"
@@ -56,24 +12,5 @@ resource "helm_release" "lnd" {
   depends_on = [
     kubernetes_secret.bitcoind,
     helm_release.bitcoind
-  ]
-}
-
-resource "helm_release" "lnd-watchonly" {
-  name      = "lnd2"
-  chart     = "${path.module}/../../charts/lnd-watchonly"
-  namespace = kubernetes_namespace.bitcoin.metadata[0].name
-
-  dependency_update = true
-  timeout           = local.bitcoin_network == "regtest" ? 900 : 9000
-  values = [
-    file("${path.module}/lnd-watchonly-${var.bitcoin_network}-values.yml")
-  ]
-
-  depends_on = [
-    kubernetes_secret.bitcoind,
-    helm_release.bitcoind,
-    kubernetes_secret.signer_credentials,
-    kubernetes_secret.signer_accounts
   ]
 }
