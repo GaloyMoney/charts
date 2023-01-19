677ec019b80b1ce8cb5311ad8737e45ec10777d3
openoms <oms@tuta.io>
2023-01-19 18:25:04 +0000

    revert test changes

--- dev/bitcoin/loop1.tf
+++ dev/bitcoin/loop1.tf
@@ -1,6 +1,5 @@
-
 resource "helm_release" "loop" {
-  count     = 0
+  count     = var.bitcoin_network == "signet" ? 0 : 1
   name      = "loop1"
   chart     = "${path.module}/../../charts/loop"
   namespace = kubernetes_namespace.bitcoin.metadata[0].name
