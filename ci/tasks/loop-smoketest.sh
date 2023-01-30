81c9d8f686aedf1b88286c8f5c7f50a259218ed6
openoms <43343391+openoms@users.noreply.github.com>
2023-01-30 11:00:57 +0000

    chore: add smoketest for Loop and RTL in actions (#2419)

--- ci/tasks/loop-smoketest.sh
+++ ci/tasks/loop-smoketest.sh
@@ -18,16 +18,3 @@ if [ "${loop_api_host}" != "" ]; then
 
     if [ "$success" != "true" ]; then echo "Could not connect to the GRPC loop_api_endpoint" && exit 1; fi;
 fi
-
-if [ "${loop_p2p_host}" != "" ]; then
-  set +e
-  for i in {1..60}; do
-    echo "Attempt ${i} to connect the REST loop_api_endpoint"
-    nc -zv ${loop_p2p_host} 8081
-    if [ $? = 0 ]; then success="true"; break; fi;
-      sleep 1
-    done
-    set -e
-
-    if [ "$success" != "true" ]; then echo "Could not connect to the REST loop_api_endpoint" && exit 1; fi;
-fi
