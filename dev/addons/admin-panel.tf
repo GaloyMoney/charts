6fb8d6ed800f9580cc1b3cc79bc61c937ef3a37a
Nick Theile <ntheile@gmail.com>
2023-02-23 16:10:47 -0600

    chore: remove admin panel stuff

--- dev/addons/admin-panel.tf
+++ dev/addons/admin-panel.tf
@@ -3,9 +3,6 @@ resource "helm_release" "admin_panel" {
   chart      = "${path.module}/../../charts/admin-panel"
   repository = "https://galoymoney.github.io/charts/"
   namespace  = kubernetes_namespace.addons.metadata[0].name
-  values = [
-    file("${path.module}/admin-panel-values.yml")
-  ]
 }
 
 resource "kubernetes_secret" "admin_panel_smoketest" {
