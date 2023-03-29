af27c4c34519bce0094cb949d6a584ad6f55c224
Sandipan Dey <hey@sandipan.dev>
2023-03-29 13:18:14 +0530

    feat: added bria to dev

---
+++ dev/bitcoin/bria.tf
@@ -0,0 +1,29 @@
+resource "kubernetes_secret" "bria_secrets" {
+  metadata {
+    name      = "bria"
+    namespace = kubernetes_namespace.bitcoin.metadata[0].name
+  }
+
+  data = {
+    "pg-con" = "postgres://bria:bria@bria-postgresql.${kubernetes_namespace.bitcoin.metadata[0].name}.cluster.svc.local:5432/bria"
+  }
+}
+
+resource "helm_release" "bria" {
+  name      = "bria"
+  chart     = "${path.module}/../../charts/bria"
+  namespace = kubernetes_namespace.bitcoin.metadata[0].name
+
+  values = [
+    file("${path.module}/bria-values.yml")
+  ]
+
+  depends_on = [
+    helm_release.bitcoind,
+    helm_release.fulcrum,
+    helm_release.lnd,
+    kubernetes_secret.bria_secrets
+  ]
+
+  dependency_update = true
+}
