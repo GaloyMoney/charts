1c8dcc27f62b7ea2401c2fbc340a30bc63c8c90a
openoms <oms@tuta.io>
2023-05-01 10:02:10 +0100

    fix: remove bitcoind-onchain from ci

--- dev/bitcoin/bitcoind.tf
+++ dev/bitcoin/bitcoind.tf
@@ -22,28 +22,3 @@ resource "helm_release" "bitcoind" {
     kubernetes_secret.bitcoind
   ]
 }
-
-resource "kubernetes_secret" "bitcoind_onchain" {
-  metadata {
-    name      = "bitcoind-onchain-rpcpassword"
-    namespace = kubernetes_namespace.bitcoin.metadata[0].name
-  }
-
-  data = {
-    password = local.bitcoind_rpcpassword
-  }
-}
-
-resource "helm_release" "bitcoind_onchain" {
-  name      = "bitcoind-onchain"
-  chart     = "${path.module}/../../charts/bitcoind"
-  namespace = kubernetes_namespace.bitcoin.metadata[0].name
-
-  values = [
-    file("${path.module}/bitcoind-${var.bitcoin_network}-values.yml")
-  ]
-
-  depends_on = [
-    kubernetes_secret.bitcoind_onchain
-  ]
-}
