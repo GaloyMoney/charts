470819d9554c627dce5f8dd68e6c5a9faa6337bc
openoms <oms@tuta.io>
2023-05-11 15:58:28 +0100

    fix: remove double base64 encoding from descriptor

--- dev/bitcoin/bitcoind.tf
+++ dev/bitcoin/bitcoind.tf
@@ -30,7 +30,7 @@ resource "kubernetes_secret" "bitcoind_signer_descriptor" {
   }
 
   data = {
-    descriptor_json_base64 = base64encode(file("${path.module}/bitcoind_signers_descriptors.json"))
+    descriptor_json = file("${path.module}/bitcoind_signers_descriptors.json")
   }
 }
 
