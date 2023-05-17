a9610c04bff20b637cf2eca287f43a393334f6db
openoms <oms@tuta.io>
2023-05-17 07:38:16 +0100

    fix: pin gcp provider version, testflight values

--- ci/testflight/mempool/main.tf
+++ ci/testflight/mempool/main.tf
@@ -86,3 +86,16 @@ provider "helm" {
     cluster_ca_certificate = base64decode(data.google_container_cluster.primary.master_auth.0.cluster_ca_certificate)
   }
 }
+
+terraform {
+  required_providers {
+    google-beta = {
+      source  = "hashicorp/google-beta"
+      version = "4.64.0"
+    }
+    google = {
+      source  = "hashicorp/google"
+      version = "4.64.0"
+    }
+  }
+}
