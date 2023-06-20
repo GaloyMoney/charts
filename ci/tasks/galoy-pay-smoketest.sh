74a46899bf52e3b2dbeee9e02136c4bd69cd3532
Kartik Shah <krtk.6160@gmail.com>
2023-06-21 03:00:01 +0530

    ci: smoketest all pay hosts

--- ci/tasks/galoy-pay-smoketest.sh
+++ ci/tasks/galoy-pay-smoketest.sh
@@ -4,25 +4,29 @@ set -eu
 
 source smoketest-settings/helpers.sh
 
-host=`setting "galoy_pay_endpoint"`
+hosts=`setting "galoy_pay_endpoints"`
 port=`setting "galoy_pay_port"`
 
 set +e
-for i in {1..15}; do
-  echo "Attempt ${i} to curl galoy pay"
-  curl --location -f ${host}:${port}
-  if [[ $? == 0 ]]; then success="true"; break; fi;
-  sleep 1
+for host in $(echo $hosts | jq -r '.[]'); do
+  for i in {1..15}; do
+    echo "Attempt ${i} to curl galoy pay"
+    curl --location -f ${host}:${port}
+    if [[ $? == 0 ]]; then success="true"; break; fi;
+    sleep 1
+  done
 done
 set -e
 
 set +e
-for i in {1..15}; do
-  echo "Attempt ${i} to curl lnurlp endpoint"
-  response=$(curl --location -fs ${host}/.well-known/lnurlp/test)
-  is_response_valid=$(echo $response | jq -r 'has("callback") and has("minSendable") and has("maxSendable") and has("metadata") and has("tag")')
-  if [[ "$is_response_valid" == "true" ]]; then success="true"; break; fi;
-  sleep 1
+for host in $(echo $hosts | jq -r '.[]'); do
+  for i in {1..15}; do
+    echo "Attempt ${i} to curl lnurlp endpoint"
+    response=$(curl --location -fs ${host}/.well-known/lnurlp/test)
+    is_response_valid=$(echo $response | jq -r 'has("callback") and has("minSendable") and has("maxSendable") and has("metadata") and has("tag")')
+    if [[ "$is_response_valid" == "true" ]]; then success="true"; break; fi;
+    sleep 1
+  done
 done
 set -e
 
