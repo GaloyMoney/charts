f136ca9b85a03013aa152b03e69f615e56c2cdee
openoms <oms@tuta.io>
2023-06-21 14:53:37 +0100

    chore: add kafka-connect smoketest and testflight

---
+++ ci/testflight/kafka-connect/main.tf
@@ -0,0 +1,81 @@
+variable "testflight_namespace" {}
+
+locals {
+  cluster_name     = "galoy-staging-cluster"
+  cluster_location = "us-east1"
+  gcp_project      = "galoy-staging"
+
+  smoketest_namespace  = "galoy-staging-smoketest"
+  testflight_namespace = var.testflight_namespace
+}
+
+resource "kubernetes_namespace" "testflight" {
+  metadata {
+    name = local.testflight_namespace
+  }
+}
+
+resource "kubernetes_secret" "smoketest" {
+  metadata {
+    name      = local.testflight_namespace
+    namespace = local.smoketest_namespace
+  }
+  data = {
+    kafka_connect_api_host = "kafka-connect-api.${local.testflight_namespace}.svc.cluster.local"
+    kafka_connect_api_port = 8083
+  }
+}
+
+resource "helm_release" "kafka_connect" {
+  name       = "kafka-connect"
+  chart      = "${path.module}/chart"
+  repository = "https://galoymoney.github.io/charts/"
+  namespace  = kubernetes_namespace.testflight.metadata[0].name
+
+  values = [
+    file("${path.module}/testflight-values.yml")
+  ]
+}
+
+data "google_container_cluster" "primary" {
+  project  = local.gcp_project
+  name     = local.cluster_name
+  location = local.cluster_location
+}
+
+data "google_client_config" "default" {
+  provider = google-beta
+}
+
+provider "kubernetes" {
+  host                   = "https://${data.google_container_cluster.primary.private_cluster_config.0.private_endpoint}"
+  token                  = data.google_client_config.default.access_token
+  cluster_ca_certificate = base64decode(data.google_container_cluster.primary.master_auth.0.cluster_ca_certificate)
+}
+
+provider "kubernetes-alpha" {
+  host                   = "https://${data.google_container_cluster.primary.private_cluster_config.0.private_endpoint}"
+  token                  = data.google_client_config.default.access_token
+  cluster_ca_certificate = base64decode(data.google_container_cluster.primary.master_auth.0.cluster_ca_certificate)
+}
+
+provider "helm" {
+  kubernetes {
+    host                   = "https://${data.google_container_cluster.primary.private_cluster_config.0.private_endpoint}"
+    token                  = data.google_client_config.default.access_token
+    cluster_ca_certificate = base64decode(data.google_container_cluster.primary.master_auth.0.cluster_ca_certificate)
+  }
+}
+
+terraform {
+  required_providers {
+    google-beta = {
+      source  = "hashicorp/google-beta"
+      version = "4.64.0"
+    }
+    google = {
+      source  = "hashicorp/google"
+      version = "4.64.0"
+    }
+  }
+}
--- dev/kafka-connect/main.tf
+++ dev/kafka-connect/main.tf
@@ -3,7 +3,19 @@ variable "name_prefix" {
 }
 
 locals {
-  kafka_namespace = "${var.name_prefix}-kafka"
+  kafka_namespace     = "${var.name_prefix}-kafka"
+  smoketest_namespace = "${var.name_prefix}-smoketest"
+}
+
+resource "kubernetes_secret" "kafka_connect_smoketest" {
+  metadata {
+    name      = "kafka-connect-smoketest"
+    namespace = local.smoketest_namespace
+  }
+  data = {
+    kafka_connect_api_host = "kafka-connect-api.${local.kafka_namespace}.svc.cluster.local"
+    kafka_connect_api_port = 8083
+  }
 }
 
 resource "helm_release" "kafka_connect" {
