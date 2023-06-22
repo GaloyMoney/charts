c1521073f75747e981be4e100cb0a19acc5a4d3e
Kartik Shah <krtk.6160@gmail.com>
2023-06-23 00:34:09 +0530

    ci: fix pay smoketest

--- ci/tasks/galoy-pay-smoketest.sh
+++ ci/tasks/galoy-pay-smoketest.sh
@@ -9,25 +9,27 @@ port=`setting "galoy_pay_port"`
 
 set +e
 for host in $(echo $hosts | jq -r '.[]'); do
+  galoy_pay_success="false"
+  lnurlp_endpoint_success="false"
+
   for i in {1..15}; do
-    echo "Attempt ${i} to curl galoy pay"
+    echo "Attempt ${i} to curl galoy pay on host ${host}"
     curl --location -f ${host}:${port}
-    if [[ $? == 0 ]]; then success="true"; break; fi;
+    if [[ $? == 0 ]]; then galoy_pay_success="true"; break; fi;
     sleep 1
   done
-done
-set -e
 
-set +e
-for host in $(echo $hosts | jq -r '.[]'); do
   for i in {1..15}; do
-    echo "Attempt ${i} to curl lnurlp endpoint"
+    echo "Attempt ${i} to curl lnurlp endpoint on host ${host}"
     response=$(curl --location -fs ${host}/.well-known/lnurlp/test)
     is_response_valid=$(echo $response | jq -r 'has("callback") and has("minSendable") and has("maxSendable") and has("metadata") and has("tag")')
-    if [[ "$is_response_valid" == "true" ]]; then success="true"; break; fi;
+    if [[ "$is_response_valid" == "true" ]]; then lnurlp_endpoint_success="true"; break; fi;
     sleep 1
   done
+
+  if [[ "$galoy_pay_success" != "true" || "$lnurlp_endpoint_success" != "true" ]]; then
+    echo "Smoke test failed for host ${host}"
+    exit 1
+  fi;
 done
 set -e
-
-if [[ "$success" != "true" ]]; then echo "Smoke test failed" && exit 1; fi;
