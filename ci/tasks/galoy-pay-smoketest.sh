45a02d2570d6367e5968917c184ace047bfdca82
Kartik Shah <krtk.6160@gmail.com>
2023-06-23 12:51:01 +0530

    ci: fix pay smoketest (#3530)
    * ci: fix pay smoketest
    
    * ci(dev): disable lnurl check
    
    * ci(testflight): disable lnurl check

--- ci/tasks/galoy-pay-smoketest.sh
+++ ci/tasks/galoy-pay-smoketest.sh
@@ -7,27 +7,41 @@ source smoketest-settings/helpers.sh
 hosts=`setting "galoy_pay_endpoints"`
 port=`setting "galoy_pay_port"`
 
+# Check if lnurl_check setting exists and set lnurl_check_disabled accordingly
+if [[ $(settings_exists "lnurl_check_disabled") == "null" ]]; then
+  lnurl_check_disabled="false"
+else
+  lnurl_check_disabled=`setting "lnurl_check_disabled"`
+fi
+
+
 set +e
 for host in $(echo $hosts | jq -r '.[]'); do
+  galoy_pay_success="false"
+  lnurlp_endpoint_success="false"
+
   for i in {1..15}; do
-    echo "Attempt ${i} to curl galoy pay"
+    echo "Attempt ${i} to curl galoy pay on host ${host}"
     curl --location -f ${host}:${port}
-    if [[ $? == 0 ]]; then success="true"; break; fi;
+    if [[ $? == 0 ]]; then galoy_pay_success="true"; break; fi;
     sleep 1
   done
+
+
+  if [[ "$lnurl_check_disabled" != "true" ]]; then
+    lnurlp_endpoint_success="false"
+    for i in {1..15}; do
+      echo "Attempt ${i} to curl lnurlp endpoint on host ${host}"
+      response=$(curl --location -fs ${host}/.well-known/lnurlp/test)
+      is_response_valid=$(echo $response | jq -r 'has("callback") and has("minSendable") and has("maxSendable") and has("metadata") and has("tag")')
+      if [[ "$is_response_valid" == "true" ]]; then lnurlp_endpoint_success="true"; break; fi;
+      sleep 1
+    done
+  fi
+
+  if [[ "$galoy_pay_success" != "true" || "$lnurlp_endpoint_success" != "true" ]]; then
+    echo "Smoke test failed for host ${host}"
+    exit 1
+  fi;
 done
 set -e
-
-set +e
-for host in $(echo $hosts | jq -r '.[]'); do
-  for i in {1..15}; do
-    echo "Attempt ${i} to curl lnurlp endpoint"
-    response=$(curl --location -fs ${host}/.well-known/lnurlp/test)
-    is_response_valid=$(echo $response | jq -r 'has("callback") and has("minSendable") and has("maxSendable") and has("metadata") and has("tag")')
-    if [[ "$is_response_valid" == "true" ]]; then success="true"; break; fi;
-    sleep 1
-  done
-done
-set -e
-
-if [[ "$success" != "true" ]]; then echo "Smoke test failed" && exit 1; fi;
--- dev/addons/galoy-pay.tf
+++ dev/addons/galoy-pay.tf
@@ -14,8 +14,9 @@ resource "kubernetes_secret" "galoy_pay_smoketest" {
     namespace = local.smoketest_namespace
   }
   data = {
-    galoy_pay_endpoints = jsonencode(["galoy-pay.${local.addons_namespace}.svc.cluster.local"])
-    galoy_pay_port      = 80
+    galoy_pay_endpoints  = jsonencode(["galoy-pay.${local.addons_namespace}.svc.cluster.local"])
+    galoy_pay_port       = 80
+    lnurl_check_disabled = "true"
   }
 }
 
