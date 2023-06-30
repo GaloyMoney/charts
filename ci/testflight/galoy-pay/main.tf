a4dd18b4c3da55ce0ff9be0c4034719049d1f939
ntheile <ntheile@gmail.com>
2023-06-29 20:30:29 -0500

    feat: websocket url

--- ci/testflight/galoy-pay/main.tf
+++ ci/testflight/galoy-pay/main.tf
@@ -10,7 +10,7 @@ locals {
   galoy_namespace      = "galoy-staging-galoy"
   testflight_namespace = var.testflight_namespace
   graphql_hostname     = "api.${local.galoy_namespace}.svc.cluster.local"
-  websocket_hostname   = "ws.${local.galoy_namespace}.svc.cluster.local"
+  websocket_url   = "wss://ws.${local.galoy_namespace}.svc.cluster.local/graphql"
 }
 
 resource "kubernetes_namespace" "testflight" {
@@ -41,7 +41,7 @@ resource "helm_release" "galoy_pay" {
       redis_namespace : "${local.galoy_namespace}",
       graphql_hostname : local.graphql_hostname,
       graphql_hostname_internal : local.graphql_hostname,
-      websocket_hostname : local.websocket_hostname,
+      websocket_url : local.websocket_url,
     })
   ]
 }
