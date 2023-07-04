f3adb62c3499a4b00e3349698d2314ab0deefab7
openoms <oms@tuta.io>
2023-07-04 12:53:19 +0100

    fix: mongo config

--- dev/kafka-connect/kafka-source-mongodb-galoy.tf
+++ dev/kafka-connect/kafka-source-mongodb-galoy.tf
@@ -1,7 +1,13 @@
 locals {
   galoy_namespace        = "${var.name_prefix}-galoy"
-  mongodb_password       = "password" #data.kubernetes_secret.mongodb_creds.data["mongodb-password"]
-  mongodb_connection_uri = "mongodb://testGaloy:${local.mongodb_password}@galoy-mongodb.${local.galoy_namespace}.svc.cluster.local:27017/?authSource=galoy&replicaSet=rs0"
+  mongodb_password       = data.kubernetes_secret.mongodb_creds.data["mongodb-password"]
+  mongodb_connection_uri = sensitive("mongodb://testGaloy:${local.mongodb_password}@galoy-mongodb-headless.${local.galoy_namespace}.svc.cluster.local:27017/?authSource=galoy&replicaSet=rs0")
+  collections = [
+    "medici_balances",
+    "medici_journals",
+    "medici_transaction_metadatas",
+    "medici_transactions"
+  ]
 }
 
 data "kubernetes_secret" "mongodb_creds" {
@@ -11,7 +17,17 @@ data "kubernetes_secret" "mongodb_creds" {
   }
 }
 
-resource "kubernetes_manifest" "kafka_source_mongo_medici_balances" {
+resource "kafka_topic" "kafka_source_mongo" {
+  for_each = toset(local.collections)
+
+  name               = "mongodb_galoy_${each.value}"
+  partitions         = 1
+  replication_factor = 3
+}
+
+resource "kubernetes_manifest" "kafka_source_mongo" {
+  for_each = toset(local.collections)
+
   manifest = {
     apiVersion = "kafka.strimzi.io/v1beta2"
     kind       = "KafkaConnector"
@@ -27,11 +43,13 @@ resource "kubernetes_manifest" "kafka_source_mongo_medici_balances" {
       tasksMax = 1
       config = {
         "startup.mode"                   = "latest",
-        "connection.uri"                 = "${local.mongodb_connection_uri}",
+        "connection.uri"                 = "password:file:/opt/kafka/external-configuration/mongodb-uri/uri"
+        "config.providers"               = "file"
+        "config.providers.file.class"    = "org.apache.kafka.common.config.provider.FileConfigProvider"
         "topic.prefix"                   = "mongodb",
         "database"                       = "galoy",
         "topic.separator"                = "_",
-        "collection"                     = "medici_balances",
+        "collection"                     = "${each.value}",
         "output.format.key"              = "schema",
         "output.format.value"            = "schema",
         "output.schema.infer.value"      = true,
@@ -41,104 +59,7 @@ resource "kubernetes_manifest" "kafka_source_mongo_medici_balances" {
         "value.converter"                = "org.apache.kafka.connect.json.JsonConverter"
       }
     }
+
   }
 }
 
-resource "kubernetes_manifest" "kafka_source_mongo_medici_journals" {
-  manifest = {
-    apiVersion = "kafka.strimzi.io/v1beta2"
-    kind       = "KafkaConnector"
-    metadata = {
-      name      = "kafka-source-mongo-medici-journals"
-      namespace = local.kafka_namespace
-      labels = {
-        "strimzi.io/cluster" = "kafka"
-      }
-    }
-    spec = {
-      class    = "com.mongodb.kafka.connect.MongoSourceConnector"
-      tasksMax = 1
-      config = {
-        "startup.mode"                   = "latest",
-        "connection.uri"                 = "${local.mongodb_connection_uri}",
-        "topic.prefix"                   = "mongodb",
-        "database"                       = "galoy",
-        "topic.separator"                = "_",
-        "collection"                     = "medici_journals",
-        "output.format.key"              = "schema",
-        "output.format.value"            = "schema",
-        "output.schema.infer.value"      = true,
-        "key.converter.schemas.enable"   = true,
-        "value.converter.schemas.enable" = true,
-        "key.converter"                  = "org.apache.kafka.connect.json.JsonConverter",
-        "value.converter"                = "org.apache.kafka.connect.json.JsonConverter"
-      }
-    }
-  }
-}
-
-resource "kubernetes_manifest" "kafka_source_mongo_medici_transaction-metadatas" {
-  manifest = {
-    apiVersion = "kafka.strimzi.io/v1beta2"
-    kind       = "KafkaConnector"
-    metadata = {
-      name      = "kafka-source-mongo-medici-transaction-metadatas"
-      namespace = local.kafka_namespace
-      labels = {
-        "strimzi.io/cluster" = "kafka"
-      }
-    }
-    spec = {
-      class    = "com.mongodb.kafka.connect.MongoSourceConnector"
-      tasksMax = 1
-      config = {
-        "startup.mode"                   = "latest",
-        "connection.uri"                 = "${local.mongodb_connection_uri}",
-        "topic.prefix"                   = "mongodb",
-        "database"                       = "galoy",
-        "topic.separator"                = "_",
-        "collection"                     = "medici_transaction_metadatas",
-        "output.format.key"              = "schema",
-        "output.format.value"            = "schema",
-        "output.schema.infer.value"      = true,
-        "key.converter.schemas.enable"   = true,
-        "value.converter.schemas.enable" = true,
-        "key.converter"                  = "org.apache.kafka.connect.json.JsonConverter",
-        "value.converter"                = "org.apache.kafka.connect.json.JsonConverter"
-      }
-    }
-  }
-}
-
-resource "kubernetes_manifest" "kafka_source_mongo_medici_transactions" {
-  manifest = {
-    apiVersion = "kafka.strimzi.io/v1beta2"
-    kind       = "KafkaConnector"
-    metadata = {
-      name      = "kafka-source-mongo-medici-transactions"
-      namespace = local.kafka_namespace
-      labels = {
-        "strimzi.io/cluster" = "kafka"
-      }
-    }
-    spec = {
-      class    = "com.mongodb.kafka.connect.MongoSourceConnector"
-      tasksMax = 1
-      config = {
-        "startup.mode"                   = "latest",
-        "connection.uri"                 = "${local.mongodb_connection_uri}",
-        "topic.prefix"                   = "mongodb",
-        "database"                       = "galoy",
-        "topic.separator"                = "_",
-        "collection"                     = "medici_transactions",
-        "output.format.key"              = "schema",
-        "output.format.value"            = "schema",
-        "output.schema.infer.value"      = true,
-        "key.converter.schemas.enable"   = true,
-        "value.converter.schemas.enable" = true,
-        "key.converter"                  = "org.apache.kafka.connect.json.JsonConverter",
-        "value.converter"                = "org.apache.kafka.connect.json.JsonConverter"
-      }
-    }
-  }
-}
