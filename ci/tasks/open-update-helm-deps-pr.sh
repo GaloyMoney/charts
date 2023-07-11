6875bef58a9519af2e353cabf2eb5c4172cfcba3
bodymindarts <justin@galoy.io>
2023-07-11 16:57:46 +0200

    ci: use github app for opening prs

--- ci/tasks/open-update-helm-deps-pr.sh
+++ ci/tasks/open-update-helm-deps-pr.sh
@@ -8,6 +8,8 @@ cat <<EOF >> ../body.md
 This PR updates Helm Chart Dependencies.
 EOF
 
+export GH_TOKEN="$(ghtoken generate -b "${GH_APP_PRIVATE_KEY}" -i "${GH_APP_ID}" | jq -r '.token')"
+
 gh pr close ${BOT_BRANCH} || true
 gh pr create \
   --title "chore(deps): update $DEP helm chart in $DIR" \
