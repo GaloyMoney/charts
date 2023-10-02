498fd34f5561d6072070197c5551b44d441e4c82
Kartik Shah <krtk.6160@gmail.com>
2023-10-02 19:59:27 +0530

    ci(dev): admin panel fix

--- dev/addons/admin-panel.tf
+++ dev/addons/admin-panel.tf
@@ -3,6 +3,10 @@ resource "helm_release" "admin_panel" {
   chart      = "${path.module}/../../charts/admin-panel"
   repository = "https://galoymoney.github.io/charts/"
   namespace  = kubernetes_namespace.addons.metadata[0].name
+
+  values = [
+    file("${path.module}/admin-panel-values.yml")
+  ]
 }
 
 resource "kubernetes_secret" "admin_panel_smoketest" {
@@ -13,5 +17,18 @@ resource "kubernetes_secret" "admin_panel_smoketest" {
   data = {
     admin_panel_endpoint = "admin-panel.${local.addons_namespace}.svc.cluster.local"
     admin_panel_port     = 3000
+  }
+}
+
+resource "kubernetes_secret" "admin_panel" {
+  metadata {
+    name      = "admin-panel"
+    namespace = kubernetes_namespace.addons.metadata[0].name
+  }
+
+  data = {
+    "next-auth-secret" : "dummy123",
+    "oauth-client-id" : "dummy",
+    "oauth-client-secret" : "dummy"
   }
 }
