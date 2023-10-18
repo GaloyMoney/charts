a9c23aad4ffdb3b64679555e23f372b1fe4f513a
bodymindarts <justin@galoy.io>
2023-10-18 11:18:55 +0200

    ci: add api-dashboard-smoketest.sh

---
+++ ci/tasks/api-dashboard-smoketest.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+set -eu
+
+source smoketest-settings/helpers.sh
+
+host=`setting "api_dashboard_endpoint"`
+port=`setting "api_dashboard_port"`
+
+set +e
+for i in {1..15}; do
+  echo "Attempt ${i} to curl admin panel"
+  curl --location -f ${host}:${port}
+  if [[ $? == 0 ]]; then success="true"; break; fi;
+  sleep 1
+done
+set -e
+
+if [[ "$success" != "true" ]]; then echo "Smoke test failed" && exit 1; fi;
