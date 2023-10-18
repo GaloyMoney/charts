9e344a3e47f5e77374f6ac717e9e210f1621a474
Kartik Shah <krtk.6160@gmail.com>
2023-10-18 10:18:59 +0530

    ci(dev): more smoketest refactoring

--- dev/bitcoin/main.tf
+++ dev/bitcoin/main.tf
@@ -1,24 +1,3 @@
-locals {
-  smoketest_namespace = "${var.name_prefix}-smoketest"
-}
-
-resource "kubernetes_secret" "bitcoind_smoketest" {
-  metadata {
-    name      = "bitcoind-smoketest"
-    namespace = local.smoketest_namespace
-  }
-
-  data = {
-    bitcoind_rpcpassword = local.bitcoind_rpcpassword
-    bitcoind_endpoint    = "bitcoind.${local.bitcoin_namespace}.svc.cluster.local"
-    bitcoind_port        = 18443
-    bitcoind_user        = "rpcuser"
-
-    bitcoind_onchain_rpcpassword = local.bitcoind_rpcpassword
-    bitcoind_onchain_endpoint    = "bitcoind.${local.bitcoin_namespace}.svc.cluster.local"
-  }
-}
-
 resource "kubernetes_secret" "fulcrum_smoketest" {
   metadata {
     name      = "fulcrum-smoketest"
@@ -51,27 +30,7 @@ resource "kubernetes_secret" "mempool_smoketest" {
   }
 }
 
-resource "kubernetes_secret" "lnd_smoketest" {
-  metadata {
-    name      = "lnd-smoketest"
-    namespace = local.smoketest_namespace
-  }
-  data = {
-    lnd_api_endpoint = "lnd1.${local.bitcoin_namespace}.svc.cluster.local"
-    lnd_p2p_endpoint = "lnd1-p2p.${local.bitcoin_namespace}.svc.cluster.local"
-  }
-}
 
-resource "kubernetes_secret" "loop_smoketest" {
-  count = var.bitcoin_network == "signet" ? 0 : 1
-  metadata {
-    name      = "loop-smoketest"
-    namespace = local.smoketest_namespace
-  }
-  data = {
-    loop_api_endpoint = "loop1.${local.bitcoin_namespace}.svc.cluster.local"
-  }
-}
 
 resource "kubernetes_secret" "rtl_smoketest" {
   metadata {
