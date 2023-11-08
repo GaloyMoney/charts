c6da3df3a9bb5a2f60f0a38a33d1680a929b698d
Kartik Shah <krtk.6160@gmail.com>
2023-11-08 19:18:16 +0530

    ci(dev): tiltify local dev setup (#4353)
    * ci(dev): add deps tiltfile
    
    * ci(dev): add bitcoin tiltfile
    
    * ci(dev): add galoy tiltfile
    
    * ci(dev): fix bria api secret key
    
    * ci(dev): more iteration on tilt
    
    * ci(dev): cleanup makefile
    
    * chore: cleanup tf
    
    * ci(dev): add to addons tiltfile
    
    * ci(dev): more cleanup
    
    * ci(dev): add smoketest Tiltfile
    
    * ci(dev): more smoketest refactoring
    
    * ci(dev): deps tf cleanup
    
    * ci(dev): kafka connect tiltfile
    
    * ci(dev): run tilt in smoketest
    
    * chore: remove helm release wrapper func
    
    * ci(dev): refactor copy secret helper func
    
    * ci(dev): ignore cronjob status
    
    * ci(dev): add tilt ci action
    
    * ci(dev): add helm release helper func
    
    * ci: skip dep build on `tilt down`
    
    * ci: add add_repos arg to helm release
    
    * ci: add add helm repo script
    
    * ci: fix helm release func
    
    * ci(dev): add cluster setup step
    
    * ci: use helmrelease helper in bitcoin lifecycle
    
    * ci: disable admin webhooks ingress nginx
    
    * ci: increase wait for secret timeout
    
    * ci: make dep update true by default
    
    * ci: add bitnami repo before pg install
    
    * ci: increase bitcoin timeout
    
    * ci: run tilt ci on self hosted runner
    
    * ci: remove nix install step
    
    * ci: add dockerhub login
    
    * ci: add temp sleep
    
    * ci: change helm release arg defaults
    
    * ci: remove sleep
    
    * ci: add temp sleep
    
    * ci: fix kratos pg endpoint
    
    * chore: add deps
    
    * chore: use helm resource for lnd
    
    * ci: fix kratos pg endpoint
    
    * ci: remove dep lnd
    
    * ci: remove kratos pg deps
    
    * ci: retry tilt ci
    
    * ci: sleep 60 seconds between tilt ci attempts
    
    * chore: reduce bitcoin lifecycle timeout
    
    * ci: reduce secret copy wait time
    
    * ci: add galoy smoketest step
    
    * ci: reduce kratos log levelgst
    
    * ci: add api dashboard secret
    
    * fix: missing comma
    
    * ci: disable dashboard auto secret creation
    
    * fix: kratos config
    
    * ci: remove `dev/.envrc`
    
    * ci: ignore some workloads' healthchecks
    
    * ci: remove .envrc sourcing
    
    * ci: move smoketests to tilt ci workflow
    
    * ci: move addons smoketest
    
    * ci: add dummy bria smoketest secret
    
    * ci: add cleanup after tilt ci
    
    * ci: remove .envrc sourcing
    
    * ci: remove dev smoketest workflow
    
    * ci: add monitoring to root tiltfile
    
    * ci: fix secret syntax
    
    * ci: add helm dep up in monitoring
    
    * chore: cleanup
    
    * ci: add stablesats Tiltfile
    
    * ci: add stablesats pg dep
    
    * ci: rename stablesats pg
    
    * ci: fix sts deps and config
    
    * chore: update stablesats chart.lock
    
    * ci: disable sts dealer health check
    
    * ci: cleanup
    
    * ci: more cleanup

--- dev/galoy-deps/ingress.tf
+++ dev/galoy-deps/ingress.tf
@@ -1,52 +1,3 @@
-locals {
-  letsencrypt_issuer_email = "dev@galoy.io"
-  ingress_namespace        = "${var.name_prefix}-ingress"
-  ingress_service_name     = "${var.name_prefix}-ingress"
-  jaeger_host              = "opentelemetry-collector.${local.otel_namespace}.svc.cluster.local"
-  enable_tracing           = true
-}
-
-resource "kubernetes_namespace" "ingress" {
-  metadata {
-    name = local.ingress_namespace
-    labels = {
-      type = "ingress-nginx"
-    }
-  }
-}
-
-resource "helm_release" "cert_manager" {
-  name      = "cert-manager"
-  chart     = "${path.module}/../../charts/galoy-deps"
-  namespace = kubernetes_namespace.ingress.metadata[0].name
-
-  values = [
-    file("${path.module}/cert-manager-values.yml.tmpl")
-  ]
-
-  depends_on = [
-    helm_release.kafka
-  ]
-}
-
-resource "helm_release" "ingress_nginx" {
-  name      = "ingress-nginx"
-  chart     = "${path.module}/../../charts/galoy-deps"
-  namespace = kubernetes_namespace.ingress.metadata[0].name
-
-  values = [
-    templatefile("${path.module}/ingress-values.yml.tmpl", {
-      jaeger_host    = local.jaeger_host
-      service_name   = local.ingress_service_name
-      enable_tracing = local.enable_tracing
-    })
-  ]
-
-  depends_on = [
-    helm_release.cert_manager,
-  ]
-}
-
 resource "kubernetes_manifest" "issuer" {
   manifest = {
     apiVersion = "cert-manager.io/v1"
@@ -57,7 +8,7 @@ resource "kubernetes_manifest" "issuer" {
     spec = {
       acme = {
         server = "https://acme-v02.api.letsencrypt.org/directory"
-        email  = local.letsencrypt_issuer_email
+        email  = "dev@galoy.io"
         privateKeySecretRef = {
           name = "letsencrypt-issuer"
         }
